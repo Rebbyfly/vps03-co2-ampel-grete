0b178c84bdd078447ab18b228e58b5cb9b5ff252
Leon Wischmeyer <leon@Mac.fritz.box>
2026-02-18 20:27:11 +0100

    CHANGE: Added getter and setter route for schedule logic

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { Client } = require("pg");
 require('dotenv').config();
 
 const app = express();
+app.use(express.json());
 
 const client = new Client({
   host: process.env.host,
@@ -30,9 +31,55 @@ app.get("/getBoolean", async (req, res) => {
   }
 });
 
+app.get("/getSchedule", async (req, res) => {
+  try {
+    const result = await client.query(
+      "SELECT start_time, end_time FROM public.co2_ampel_grete WHERE id = 1;"
+    )
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Not found" });
+    }
+
+    return res.json(result.rows[0]);
+  } catch (err) {
+    return res.status(500).json({ error: "Database error" });
+  }
+});
+
+
 app.post("/toggleBoolean", async (req, res) => {
   const result = await client.query("UPDATE public.co2_ampel_grete SET powered = NOT powered WHERE id = 1;");
   res.json(result.rows);
 });
+
+app.post("/setSchedule", async (req, res) => {
+  try {
+    const { startTime, endTime } = req.body;
+    if(!startTime || !endTime) {
+      return res.status(400).json({ error: "Missing fields" });
+    }
+
+    const result = await client.query(
+      `
+      UPDATE public.co2_ampel_grete
+      SET start_time = $1,
+          end_time   = $2
+      WHERE id = 1
+      RETURNING start_time, end_time;
+      `,
+      [startTime, endTime]
+    );
+
+    if(result.rowCount === 0) {
+      return res.status(404).json({ error: "Not found" });
+    }
+
+    return res.json(result.rows[0]);
+  } catch(err) {
+    return res.status(500).json({ error: "Database error" });
+  }
+});
+
 
 app.listen(3000, '0.0.0.0', () => console.log("Server läuft"));
