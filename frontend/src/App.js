64fa97d003c177380afb8b2888abe126f1a912e8
Leon Wischmeyer <leon@Mac.fritz.box>
2026-02-19 15:37:18 +0100

    FEATURE: Added React frontend for changing schedule and powermode

---
+++ frontend/src/App.js
@@ -0,0 +1,117 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+export default function App() {
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+  const [isOn, setIsOn] = useState(null);
+  const [saved, setSaved] = useState(false);
+
+
+
+  useEffect(() => {
+    // Default-Werte vom Backend laden
+    async function fetchBoolean() {
+      try {
+        const res = await fetch("http://vm-l-vps03.leon-malte.de:3000/getBoolean");
+        const data = await res.json();
+        setIsOn(data);
+      } catch (err) {
+        console.error("Fehler beim Laden:", err);
+      }
+    }
+    async function fetchSchedule() {
+      try {
+        const res = await fetch("http://vm-l-vps03.leon-malte.de:3000/getSchedule");
+        const data = await res.json();
+
+        setStartTime(data.start_time);
+        setEndTime(data.end_time);
+      } catch (err) {
+        console.error("Fehler beim Laden:", err);
+      }
+    }
+    fetchBoolean();
+    fetchSchedule();
+  }, []);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("http://vm-l-vps03.leon-malte.de:3000/setSchedule", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          startTime,
+          endTime,
+        }),
+      });
+
+      if (res.ok) {
+        setSaved(true);
+        setTimeout(() => setSaved(false), 800);
+      }
+    } catch (err) {
+      console.error("Speichern fehlgeschlagen:", err);
+    }
+  }
+
+  const toggleBoolean = async () => {
+    await fetch("http://vm-l-vps03.leon-malte.de:3000/toggleBoolean", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: '',
+    });
+  };
+
+  const onClickAnAus = () => {
+    toggleBoolean();
+    setIsOn(!isOn);
+  }
+
+  return (
+    <div className="container">
+      <form className="card" onSubmit={handleSubmit}>
+        <div className="field">
+          <h2>Startzeit</h2>
+          <input
+            type="time"
+            value={startTime}
+            onChange={(e) => setStartTime(e.target.value)}
+          />
+        </div>
+
+        <div className="field">
+          <h2>Endzeit</h2>
+          <input
+            type="time"
+            value={endTime}
+            onChange={(e) => setEndTime(e.target.value)}
+          />
+        </div>
+
+        <button
+          type="submit"
+          className={saved ? "flash" : ""}
+        >
+          {saved ? "Gespeichert ✓" : "Festlegen"}
+        </button>
+
+
+        <button
+          style={{ marginTop: "2vh" }}
+          type="button"
+          className="toggle"
+          onClick={() => onClickAnAus()}
+        >
+          {isOn ? "Aus" : "An"}
+        </button>
+      </form>
+    </div>
+  );
+}
